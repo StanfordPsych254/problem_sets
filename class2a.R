43158c07dd9b5bbde26e0c39606157c9782dec99
Michael C. Frank <mcfrank@stanford.edu>
2017-01-17 22:08:27 -0800

    class 2a demo

--- class2a.R
+++ class2a.R
@@ -1,21 +1,23 @@
-rm(list=ls())
-library(ggplot2)
-library(tidyr)
-library(dplyr)
+library(tidyverse)
 
 head(iris)
 unique(iris$Species)
 hist(iris$Sepal.Width)
 qplot(Sepal.Width, data = iris)
-qplot(Sepal.Length, data = iris)
+qplot(Sepal.Width, data = iris, binwidth = .2)
 
-tidy.iris <- iris %>%
+# gather up
+tidy_iris <- iris %>%
   mutate(index = 1:n()) %>%
   gather(measure_name, measure_value, 
          Sepal.Length, Sepal.Width, 
-         Petal.Length, Petal.Width) %>%
+         Petal.Length, Petal.Width) 
+
+# spread just for kicks
+wide_iris <- tidy_iris %>%
   spread(measure_name, measure_value)
 
+# plot
 qplot(measure_value, 
-      facets = ~ Species,
-      data = tidy.iris)
+      facets = measure_name ~ Species,
+      data = tidy_iris)
